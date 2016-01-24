a2858f94845feafb5ce34e451e4462b8dd982968
Zachary Mixon <Zach@users-MacBook-Air.local>
2016-01-24 13:51:27 -0600

    Failed attempt at four layer project

---
+++ MovieLibrary/MovieLibrary.Presentation.Web/ngApp/app.ts
@@ -0,0 +1,12 @@
+﻿namespace MovieLibrary.Presentation.Web {
+
+    angular.module("MovieLibrary.Presentation.Web", ['ngRoute'])
+        .config(function($routeProvider) {
+            //$routeProvider.when('/', {
+            // templateUrl: '/path/to/view',
+            // controller: MovieLibrary.Presentation.Web.Controllers.ControllerClass,
+            // controllerAs: 'views variable name for controller'
+            //});
+            $routeProvider.when('/', { template: 'Hello World!' });
+        });
+}
---
+++ MovieLibrary/MovieLibrary/ngApp/app.ts
@@ -0,0 +1,12 @@
+﻿namespace MovieLibrary {
+
+    angular.module("MovieLibrary", ['ngRoute'])
+        .config(function($routeProvider) {
+            //$routeProvider.when('/', {
+            // templateUrl: '/path/to/view',
+            // controller: MovieLibrary.Controllers.ControllerClass,
+            // controllerAs: 'views variable name for controller'
+            //});
+            $routeProvider.when('/', { template: 'Hello World!' });
+        });
+}
